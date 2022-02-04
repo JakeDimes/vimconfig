3ee989e15343b1b3627ad2898175fc9535b643ea
JakeDimes <jakebq2001@outlook.com>
2022-02-04 12:10:55 -0500

    Improved sync

--- sync.sh
+++ sync.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-read -p "This will overwrite directory ~/.vim and file ~/.vimrc. Continue? [y/n]" dest
+read -p "Sync To (T) or From (F) ROOT vim config? [T/F/n]" ANSWER
 
-if [ $dest == "y" ]; then
-	cp -r ./.vim $HOME/
-	cp ./.vimrc $HOME/
+if [ $ANSWER == "F" ]; then
+	read -p "This will overwrite directory PROJECT_ROOT/.vim and file PROJECT_ROOT/.virmc. Continue? [y/n]" ANSWER
+	
+	if [ $ANSWER == "y" ]; then
+
+		cp -r $HOME/.vim ./
+		cp $HOME/.vimrc ./
+
+	else
+		echo "Cancelling"
+	fi
+
+elif [ $ANSWER == "T" ]; then
+
+	read -p "This will overwrite directory ~/.vim and file ~/.vimrc. Continue? [y/n]" ANSWER
+	
+	if [ $ANSWER == "y" ]; then
+		cp -r ./.vim $HOME/
+		cp ./.vimrc $HOME/
+	else
+		echo "Cancelling"
+	fi
+
 else
-	echo "Cancelling"
+	echo "Exiting"
 fi
 
-unset dest
+unset ANSWER
 
